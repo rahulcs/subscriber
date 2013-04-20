019690ca50797aadfe3bba66bbb220a867519b12
Rahul C S <rahul@unbxd.com>
2013-04-20 17:35:02 +0530

    Adding node modules & send mail server file

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-subscriber
+Subscriber
 ==========
 
 A lightweight nodejs module for notifying new subscribers to your website.
+
+Usage
+=====
+
+TO-DO
+
+Browser Action
+==============
+
+$.ajax({
+        url: 'http://localhost:6969/subscribe',
+        dataType: 'jsonp',
+        jsonpCallback: '_mailer',
+        cache: false,
+        type: 'POST',
+        data: {"email": "john.doe@gmail.com"}
+}).always(function(data){
+        console.log(data);
+});
---
+++ sendmail.js
@@ -0,0 +1,64 @@
+var email = require("emailjs/email");
+var http = require('http');
+var url = require('url');
+var path = require('path');
+var querystring = require('querystring');
+var usrName = '';
+var passWrd = '';
+var toAdd = '';
+var argLength = process.argv.length;
+
+if(argLength <= 3 || argLength > 5 || argLength === 4){
+    console.log("Usage: node sendmail.js <username> <password> <to: address>");
+    process.exit(1);
+}
+
+process.argv.forEach(function (val, index, array) {
+    console.log(index + ': ' + val);
+    if(index === 2){
+	usrName = val;
+    } else if(index === 3){
+	passWrd = val;
+    } else if(index === 4){
+	toAdd = val;
+    }
+});
+
+var server = email.server.connect({
+    user: usrName,
+    password: passWrd,
+    host: "smtp.gmail.com",
+    ssl: true
+});
+
+http.createServer(function(req, res) {
+    var uri = url.parse(req.url).pathname;
+    console.log(uri);
+    res.writeHead(200, {'Content-Type': 'text/json'});
+    if(uri === "/subscribe"){
+	if(req.method === "POST"){
+	    var body = '';
+	    req.on('data', function(chunk){
+		body += chunk;
+	    });
+	    req.on('end', function(){
+		var postData = querystring.parse(body);
+		var  mailText = 'New User Email ID: ' + postData.email;
+		server.send({
+		    text: mailText,
+		    from: "<"+usrName+">",
+		    to: "<"+toAdd+">",
+		    subject: "New User Notification"
+		}, function(err, message){
+		    if(err === null){
+			res.end('_mailer(\'{"message": "Mail Sent successfully", "success": true}\')');
+		    } else {
+			res.end('_mailer(\'{"message": "Mail sending failed", "success": false}\')');
+		    }
+		});
+	    });
+	}
+    } else {
+	res.end('_mailer(\'{"message": "Wrong URL", "success": false}\')');
+    }
+}).listen(6969);
