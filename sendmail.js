d58590aff858e2e4f4cbb2ea1ac7645c8c5e6cd7
Rahul C S <rahul@unbxd.com>
2013-04-24 13:48:29 +0530

    Change POST/GET codepaths

--- sendmail.js
+++ sendmail.js
@@ -34,14 +34,16 @@ http.createServer(function(req, res) {
     var uri = url.parse(req.url).pathname;
     res.writeHead(200, {'Content-Type': 'text/json'});
     if(uri === "/subscribe"){
-	if(req.method === "POST" || req.method === "GET"){
+	if(req.method === "POST"){
 	    console.log('post');
 	    var body = '';
 	    req.on('data', function(chunk){
 		body += chunk;
 	    });
 	    req.on('end', function(){
+		console.log(body);
 		var postData = querystring.parse(body);
+		console.log(postData);
 		var  mailText = 'New User Email ID: ' + postData.email;
 		console.log(server, mailText, usrName, toAdd);
 		server.send({
@@ -61,6 +63,33 @@ http.createServer(function(req, res) {
 		    }
 		});
 	    });
+	} else if(req.method === "GET"){
+	    console.log('get');
+	    var url_parts = url.parse(req.url, true);
+	    var mailText = '';
+	    mailText += 'New User Email ID: '+ url_parts.query.email;
+	    mailText += '<br>' + 'First Name: ' + url_parts.query.firstName;
+	    mailText += '<br>' + 'Last Name: ' + url_parts.query.lastName;
+	    mailText += '<br>' + 'Phone: ' + url_parts.query.phone;
+	    mailText += '<br>' + 'Organization: ' + url_parts.query.organization;
+	    if(mailText !== ''){
+		server.send({
+		    text: mailText,
+		    from: "<"+usrName+">",
+		    to: "<"+toAdd+">",
+		    subject: "New User Notification",
+		    attachment: [{
+			data: "<html>"+mailText+"</html>",
+			alternative: true
+		    }]
+		}, function(err, message){
+		    if(err === null){
+			res.end('_mailer(\'{"message": "Mail Sent successfully", "success": true}\')');
+		    } else {
+			res.end('_mailer(\'{"message": "Mail sending failed", "success": false}\')');
+		    }
+		});
+	    }
 	}
     } else {
 	res.end('_mailer(\'{"message": "Wrong URL", "success": false}\')');
